d89b82b11826ad63b0ef410ae6cc88e850889ae1
Yashveer Rai <workwithyash19@gmail.com>
2026-07-28 22:47:39 +0530

    Time: 0 ms (100%), Space: 20.3 MB (74.18%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def missingNumber(self, arr: List[int]) -> int:
+        n = len(arr)
+        expected_sum = n * (n + 1) // 2
+        actual_sum = sum(arr)
+        return (expected_sum - actual_sum)
+
+        
