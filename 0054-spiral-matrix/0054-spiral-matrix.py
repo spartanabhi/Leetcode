9310ab86b6824d2e3758690839ce407ccfaf5a67
Yashveer Rai <workwithyash19@gmail.com>
2026-08-14 03:14:48 +0530

    Time: 3 ms (1.55%), Space: 19.2 MB (72.83%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -1,30 +1,4 @@
-class Solution(object):
+class Solution:
     def spiralOrder(self, matrix):
-        """
-        :type matrix: List[List[int]]
-        :rtype: List[int]
-        """
-        min_col = min_row = 0
-        max_col , max_row = len(matrix[0]), len(matrix)
-        row = 0
-        output = []
+        return matrix and [*matrix.pop(0)] + self.spiralOrder([*zip(*matrix)][::-1])
         
-
-        while min_col<max_col and min_row<max_row:
-            for col in range(min_col,max_col):
-                output.append(matrix[row][col])
-            min_row +=1
-
-            for row in range(min_row , max_row):
-                output.append(matrix[row][col])
-            max_col -=1
-
-            if min_col<max_col and min_row<max_row:
-                for col in range(max_col-1,min_col-1,-1):
-                    output.append(matrix[row][col])
-                max_row -=1
-
-                for row in range(max_row-1,min_row-1,-1):
-                    output.append(matrix[row][col])
-                min_col +=1
-        return output
